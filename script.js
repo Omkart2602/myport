e74bbe6282c7b0633c0ff1b48b17cb393bad5ad0
Omkar Taradale <142131043+Omkart2602@users.noreply.github.com>
2024-10-16 14:07:43 +0530

    added files

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle functionality for the mobile menu
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.navbar').classList.toggle('active');
+});
